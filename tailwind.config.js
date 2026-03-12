585fe83359f2559a1fb905effc2ce88c6df25876
nitemouse1986 <4kvcp45nph@privaterelay.appleid.com>
2026-03-12 17:56:21 -0400

    Create Tailwind configuration for custom colors and fonts

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        customBlue: '#1DA1F2',
+        customGreen: '#17BF63',
+        customPurple: '#794BC4',
+      },
+      fontFamily: {
+        custom: ['Arial', 'sans-serif'],
+      },
+    },
+  },
+};
